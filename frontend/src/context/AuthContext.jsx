89b9a27f0c72665ef64a5729d196b502a5ba2c94
Kushagrakumar20 <kushagrakumar0210@gmail.com>
2026-02-19 22:16:02 +0530

    Small correction

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useReducer, useEffect } from 'react';
 // import axios from 'axios';
-import API from '../Api';
+import API from '../api';
 import toast from 'react-hot-toast';
 
 // Initial state
